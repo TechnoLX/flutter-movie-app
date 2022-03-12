62b7561bb1a31b18f2e193e30a204e767ad86dd7
TechnoLX <78125150+TechnoLX@users.noreply.github.com>
2022-03-12 19:53:44 +0800

    Create style.dart

---
+++ lib/constant/style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static const Color primaryColor = Color(0xFF151C26);
+  static const Color secondColor = Color(0xFFF4C10F);
+  static const Color textColor = Color(0xFF5A606B);
+
+  const Style();
+}
